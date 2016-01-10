642c8ce4731605fd8c580dd8a1ac1d99d1b93046
Felipe Borges <jfpsb_felipe2@hotmail.com>
2016-01-10 13:56:15 -0300

    sss

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="resources/css/index.css">
+	<title>LinkinParkTVBr</title>
+</head>
+<body>
+	<article class="index-article">
+		<div class="videos-header">
+			<h2 class="header">Vídeos Recentes</h2>
+		</div>
+		<article class="video-grid">
+		
+			
+		
+		</article>
+	</article>
+</body>
+</html>
---
+++ mus-leg.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Músicas Legendadas</title>
+</head>
+<body>
+	<article>
+		sadasd
+		asd
+		asd
+		as
+		da
+		sd
+		asd
+		
+	</article>
+</body>
+</html>

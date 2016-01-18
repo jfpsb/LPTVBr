acd22b198719f22334327a183c6b10d1908fcdc9
Felipe Borges <jfpsb_felipe2@hotmail.com>
2016-01-18 20:18:07 -0300

    altnc single

---
+++ musicas/singles/a-light-that-never-comes/a-light-that-never-comes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="shortcut icon" href="/LPTVBr/favicon.ico" type="image/x-icon" />
+<link id="favicon" href="/LPTVBr/favicon.ico" rel="shortcut icon"
+	type="image/vnd.microsoft.icon"></link>
+</head>
+<body>
+	<?php
+	$videoId = 'lID2I6GAcwE';
+	$page_title = "A Light That Never Comes (Tradução)";
+	$musica = "a-light-that-never-comes";
+	$nomeMusica = "A Light That Never Comes";
+	$aside = "singles/a-light-that-never-comes.php";
+	$album = '';
+	include_once '../../traducao-template.php';
+	?>
+</body>
+</html>
---
+++ template/aside/singles/a-light-that-never-comes.php
@@ -0,0 +1,9 @@
+<div class="album-panel">
+	<img src="/LPTVBr/resources/imagens/singles/cover250/a-light-that-never-comes.jpg"
+		class="album-cover"> <a class="album-link"
+		href="/LPTVBr/musicas/singles/a-light-that-never-comes">A Light That Never Comes</a>
+	<ol class="album-lista">
+		<li class="traducao-li"><a class="album-text"
+			href="/LPTVBr/musicas/lpu/a-light-that-never-comes/a-light-that-never-comes.php">1. A Light That Never Comes</a></li>
+	</ol>
+</div>

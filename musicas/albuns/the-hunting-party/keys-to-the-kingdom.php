e9988ed8697f28c88cc620e4cd371fd80a09780b
jfpsb <jfpsb_felipe2@hotmail.com>
2016-02-02 11:20:50 -0200

    Inicio JSON

--- musicas/albuns/the-hunting-party/keys-to-the-kingdom.php
+++ musicas/albuns/the-hunting-party/keys-to-the-kingdom.php
@@ -11,12 +11,24 @@
 </head>
 <body>
 	<?php
-	$page_title = "The Hunting Party : Keys To The Kingdom (Tradução)";
-	$videoId = 'ZPlUjhroNd4';
-	$musica = "keys-to-the-kingdom";
-	$nomeMusica = "Keys To The Kingdom";
-	$aside = "the-hunting-party";
-	$album = 'thp';
+// 	$page_title = "The Hunting Party : Keys To The Kingdom (Tradução)";
+// 	$videoId = 'ZPlUjhroNd4';
+// 	$musica = "keys-to-the-kingdom";
+// 	$nomeMusica = "Keys To The Kingdom";
+// 	$aside = "the-hunting-party";
+// 	$album = 'thp';
+	$str = file_get_contents("http://localhost/LPTVBr/musicas/albuns/albuns.json");
+	$str = utf8_encode($str);
+	
+	$lista = array();
+	$list = $str->fetch_all();
+	$str->free_result();
+	$str->close();
+	$thp = json_decode($lista, true);
+	
+	echo $thp['title'];
+	
+	$page_title = $thp['title']; 
 	include_once 'musicas/traducao-template.php';
 	?>
 </body>

e0b9b6e585a85f89000051c7450da7062c1dd3bf
jfpsb <jfpsb_felipe2@hotmail.com>
2016-02-03 17:26:05 -0200

    Mais lpu

--- musicas/lpu/lpu10.php
+++ musicas/lpu/lpu10.php
@@ -2,25 +2,6 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="shortcut icon" href="/LPTVBr/favicon.ico" type="image/x-icon" />
-<link id="favicon" href="/LPTVBr/favicon.ico" rel="shortcut icon"
-	type="image/vnd.microsoft.icon"></link>
-<link rel="shortcut icon" href="/LPTVBr/favicon.ico" type="image/x-icon" />
-<link id="favicon" href="/LPTVBr/favicon.ico" rel="shortcut icon"
-	type="image/vnd.microsoft.icon"></link>
-<script type="text/javascript"
-	src="/LPTVBr/resources/js/jquery-1.11.3.min.js"></script>
-<link rel="stylesheet" type="text/css"
-	href="/LPTVBr/resources/css/mus-lista.css">
-<link rel="stylesheet" type="text/css"
-	href="/LPTVBr/resources/css/mus-lista-bg.css">
-<link rel="stylesheet" type="text/css"
-	href="/LPTVBr/resources/css/global.css">
-<script type="text/javascript"
-	src="/LPTVBr/resources/js/focusMainSection.js"></script>
-<script>
-	document.title = 'LPU :  LP Underground 10';
-</script>
 </head>
 <body>
 	<article id="lpu10-article" class="table-article">

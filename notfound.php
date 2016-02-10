49197aa52589939c6a6f6b420276d25d081d9403
Felipe Borges <jfpsb_felipe2@hotmail.com>
2016-02-10 02:05:44 -0300

    Pagina notfound modificada

--- notfound.php
+++ notfound.php
@@ -4,14 +4,21 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-<link id="favicon" href="/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon"></link>
+<link id="favicon" href="/favicon.ico" rel="shortcut icon"
+	type="image/vnd.microsoft.icon"></link>
 <title>Página Não Encontrada em LPTVBr</title>
 </head>
-<body style="background: black;">
-	<div align="center">
-		<a href="<?php echo "http://" . $_SERVER['SERVER_NAME']?>">
-			<img alt="" src="/resources/imagens/notfound.jpg" style="max-width: 100%; border: 1px groove gray;">
-		</a>
+<body style="background: #EEEEEE">
+	<div align="center" style="font-family:'TrebuchetMS', Helvetica, sans-serif; font-size: 15px;">
+		<img src="/resources/imagens/banner320.jpg">
+		<h1>Conteúdo não encontrado.</h1>
+		<h2>Talvez o link para a página tenha mudado ou ela não existe mesmo
+			=(</h2>
+		<h2>
+			<a href="<?php echo "http://" . $_SERVER['SERVER_NAME']?>">Aperte
+				aqui</a> e volte ao início do nosso site ou simplesmente volte para
+			a página anterior.
+		</h2>
 	</div>
 </body>
 </html>

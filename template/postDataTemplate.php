1ee6d3300fc1f8aa750d8fddb8b059fb434f04e7
Felipe Borges <jfpsb_felipe2@hotmail.com>
2016-09-25 12:04:35 -0300

    Listagem de lptvs completa

--- template/postDataTemplate.php
+++ template/postDataTemplate.php
@@ -1,5 +1,8 @@
 <div class="postData">
 	<p class="dataTexto">
+
+
+		Postado:<br>
 		<?php echo $dia?><br class="dataBr"> <?php echo $mes?><br
 			class="dataBr"> <?php echo $ano?><br class="dataBr">
 	</p>
--- videos/entrevista/entrevista.php
+++ videos/entrevista/entrevista.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="/resources/css/global.css">
 <link rel="stylesheet" type="text/css"
-	href="/resources/css/letra-musica.css">
+	href="/resources/css/release-lista.css">
 <link rel="stylesheet" href="/resources/css/jquery-ui.min.css">
 <link rel="stylesheet" href="/resources/css/primeui-2.2-min.css">
 <script type="text/javascript" src="/resources/js/jquery-1.11.3.min.js"></script>
@@ -14,23 +15,27 @@
 		$('#entrevista-tabView').puitabview();
 	});
 </script>
+<title>Entrevistas Legendadas - LPTVBr</title>
 </head>
 <body>
-	<article>
-		<div id="entrevista-tabView" class="tabView">
-			<ul class="tabs-entrevista">
-				<li><a href="#dois-mil-catorze">2014</a></li>
-				<li><a href="#dois-mil-quinze">2015</a></li>
-				<li><a href="#dois-mil-dezesseis">2016</a></li>
-			</ul>
+	<?php include_once '../../template/header.php';?>
+	
+	<section class="mainSection">
+		<article>
+			<div id="entrevista-tabView" class="tabView">
+				<ul class="tabs-entrevista">
+					<li><a href="#dois-mil-catorze">2014</a></li>
+					<li><a href="#dois-mil-quinze">2015</a></li>
+					<li><a href="#dois-mil-dezesseis">2016</a></li>
+				</ul>
 
-			<div>
-				<div id="#dois-mil-catorze">
-					<table class="panelGrid">
-						<tr>
-							<td>
-								<div class="panel">
-									<a class="link" href="2014/lightning-round-red-bull">
+				<div>
+					<div id="#dois-mil-catorze">
+						<table class="panelGrid">
+							<tr>
+								<td>
+									<div class="panel">
+										<a class="link" href="2014/lightning-round-red-bull">
 										<?php
 										$dia = '7';
 										$mes = 'Set';
@@ -38,17 +43,17 @@
 										include 'postDataTemplate.php';
 										?>
 										<img
-										alt="Linkin Park Lightning Round In The Red Bull Sound Space At KROQ"
-										class="linkImg"
-										src="/resources/imagens/videos/entrevista/lightning-round-red-bull.jpg">
-										<label class="nomeLink">Linkin Park Lightning Round In The Red
-											Bull Sound Space At KROQ</label>
-									</a>
-								</div>
-							</td>
-							<td>
-								<div class="panel">
-									<a class="link" href="2014/guitar-center-rockwalk-induction">
+											alt="Linkin Park Lightning Round In The Red Bull Sound Space At KROQ"
+											class="linkImg"
+											src="/resources/imagens/videos/entrevista/lightning-round-red-bull.jpg">
+											<label class="nomeLink">Linkin Park Lightning Round In The
+												Red Bull Sound Space At KROQ</label>
+										</a>
+									</div>
+								</td>
+								<td>
+									<div class="panel">
+										<a class="link" href="2014/guitar-center-rockwalk-induction">
 										<?php
 										$dia = '5';
 										$mes = 'Jul';
@@ -56,34 +61,37 @@
 										include 'postDataTemplate.php';
 										?>
 										<img alt="Guitar Center Rockwalk Induction" class="linkImg"
-										src="/resources/imagens/videos/entrevista/guitar-center-rockwalk-induction.jpg">
-										<label class="nomeLink">Guitar Center Rockwalk Induction</label>
-									</a>
-								</div>
-							</td>
-						</tr>
-					</table>
-				</div>
-
-				<div id="#dois-mil-quinze">
-					<div align="center">
-						<h2
-							style="font-family: 'Courier New', Courier, monospace; color: white;">
-							Ainda não disponíveis!<br>Volte em breve.
-						</h2>
+											src="/resources/imagens/videos/entrevista/guitar-center-rockwalk-induction.jpg">
+											<label class="nomeLink">Guitar Center Rockwalk Induction</label>
+										</a>
+									</div>
+								</td>
+							</tr>
+						</table>
 					</div>
-				</div>
 
-				<div id="#dois-mil-dezesseis">
-					<div align="center">
-						<h2
-							style="font-family: 'Courier New', Courier, monospace; color: white;">
-							Ainda não disponíveis!<br>Volte em breve.
-						</h2>
+					<div id="#dois-mil-quinze">
+						<div align="center">
+							<h2
+								style="font-family: 'Courier New', Courier, monospace; color: black;">
+								Ainda não disponíveis!<br>Volte em breve.
+							</h2>
+						</div>
+					</div>
+
+					<div id="#dois-mil-dezesseis">
+						<div align="center">
+							<h2
+								style="font-family: 'Courier New', Courier, monospace; color: black;">
+								Ainda não disponíveis!<br>Volte em breve.
+							</h2>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</article>
+		</article>
+	</section>
+	
+	<?php include_once '../../template/footer.php';?>
 </body>
 </html>

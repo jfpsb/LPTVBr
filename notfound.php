3dd875a44006a4c4233a8efe0bade4bd5c5ac7ac
Felipe Borges <jfpsb_felipe2@hotmail.com>
2016-10-07 19:05:07 -0300

    Redirecionando antigas páginas Consertando música em arquivos de remix Atualizando página notfound com link para o início do site

--- notfound.php
+++ notfound.php
@@ -1,13 +1,22 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>Página não encontrada em LPTVBr</title>
 </head>
 <body>
 	<div align="center"
 		style="font-family: 'TrebuchetMS', Helvetica, sans-serif; font-size: 15px; padding: 15px;">
 		<h1>Conteúdo não encontrado.</h1>
-		<h2>Esta página não existe em LPTVBr ou está temporariamente indisponível.</h2>
+		<h2>Esta página não existe em LPTVBr ou está temporariamente
+			indisponível.</h2>
+		<h2>
+			Houve uma recente mudança em nossos links, mas nossos conteúdos estão
+			todos lá. Acesse por nossa
+			<a href="/">Página
+				Inicial</a> e navegue normalmente.
+		</h2>
 	</div>
 </body>
 </html>

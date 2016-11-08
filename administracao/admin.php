076148aff3d3507d9836210dc107711951397e55
jfpsb <jfpsb_felipe2@hotmail.com>
2016-11-08 17:26:01 -0300

    Começando tela de login

---
+++ administracao/admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Administração - LPTVBr</title>
+</head>
+<body>
+<?php
+if (isset ( $_POST ['submit'] )) {
+	echo "OLAAAAAAAA";
+}
+else {
+	echo "Login nâo realizado.";
+	echo "<a href=\"/administracao/login.php\">Login</a>";
+}
+?>
+</body>
+</html>
---
+++ administracao/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Login - LPTVBr</title>
+</head>
+<body>
+	<div align="center">
+		<form action="admin.php" method="post">
+			<label for="login">Login:</label><br> <input type="text" name="login"
+				required><br> <label for="senha">Senha:</label><br> <input
+				type="password" name="senha" required> <br> <input type="submit"
+				value="Entrar" name="submit">
+		</form>
+	</div>
+</body>
+</html>

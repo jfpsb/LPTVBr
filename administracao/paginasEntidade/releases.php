c6b46acc0689e1b6fc878df9535d9a579572f47d
jfpsb <jfpsb_felipe2@hotmail.com>
2016-11-15 12:29:28 -0300

    Limitando width em tela de adm

--- administracao/paginasEntidade/releases.php
+++ administracao/paginasEntidade/releases.php
@@ -1,17 +1,10 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-</head>
-<body>
-	<article class="adminArticle">
-		<div class="adminListaWrapper">
-			<ul id="releasesLista" class="adminListas">
-				<li><button id="releaseCadastrar" class="linksLista">Cadastrar</button></li>
-				<li><button id="releaseAtualizar" class="linksLista">Atualizar</button></li>
-				<li><button id="releaseRemover" class="linksLista">Remover</button></li>
-			</ul>
-		</div>
-		<div class="adminOpcaoContent">ESCOLHA AO LADO</div>
-	</article>
-</body>
-</html>
+<article>
+	<div class="adminListaWrapper">
+		<ul id="releasesLista" class="adminListas">
+			<li><button id="releaseCadastrar" class="linksLista">Cadastrar</button></li>
+			<li><button id="releaseAtualizar" class="linksLista">Atualizar</button></li>
+			<li><button id="releaseRemover" class="linksLista">Remover</button></li>
+		</ul>
+	</div>
+	<div class="adminOpcaoContent">ESCOLHA AO LADO</div>
+</article>
--- administracao/paginasEntidade/videos.php
+++ administracao/paginasEntidade/videos.php
@@ -1,17 +1,10 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-</head>
-<body>
-	<article class="adminArticle">
-		<div class="adminListaWrapper">
-			<ul id="videosLista" class="adminListas">
-				<li><button id="videoCadastrar" class="linksLista">Cadastrar</button></li>
-				<li><button id="videoAtualizar" class="linksLista">Atualizar</button></li>
-				<li><button id="videoRemover" class="linksLista">Remover</button></li>
-			</ul>
-		</div>
-		<div class="adminOpcaoContent">ESCOLHA AO LADO</div>
-	</article>
-</body>
-</html>
+<article class="adminArticle">
+	<div class="adminListaWrapper">
+		<ul id="videosLista" class="adminListas">
+			<li><button id="videoCadastrar" class="linksLista">Cadastrar</button></li>
+			<li><button id="videoAtualizar" class="linksLista">Atualizar</button></li>
+			<li><button id="videoRemover" class="linksLista">Remover</button></li>
+		</ul>
+	</div>
+	<div class="adminOpcaoContent">ESCOLHA AO LADO</div>
+</article>
